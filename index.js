8304732e6d89a0dfb95018e50b13b146501298a1
Rahyan Shamsi <129554871+rahyanshamsithroughltd@users.noreply.github.com>
2023-12-06 19:39:42 +0600

    private route added for login in page to solve the login route issue

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 //require cors and express
-const express =require('express');
-const cors =require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 //set port for backend port 5000
 const port = process.env.PORT || 5000;
 //mongo client
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // config file require to hide the pass and user
-require('dotenv').config()
-
-
+require("dotenv").config();
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_PASS)
-
-
+console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zruwsn2.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -27,7 +23,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -35,40 +31,77 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const serviceCollection = client.db("doctorAppoint").collection("services");
+    const checkoutCollection = client
+      .db("doctorAppoint")
+      .collection("checkout");
 
-    const serviceCollection=client.db('doctorAppoint').collection('services');
-    const checkoutCollection =client.db('doctorAppoint').collection('checkout');
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.get('/services', async(req,res)=>{
-        const cursor =serviceCollection.find();
-        const result = await cursor.toArray();
-        res.send(result);
-    })
-
-    app.get('/services/:id',async(req,res)=>{
-      const id =req.params.id;
-      const query ={_id: new ObjectId(id)} 
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
 
       const options = {
-        
         // Include only the `title` fields in the returned document
-        projection: {title: 1, price: 1,service_id:1 },
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
       };
 
-      const result =await serviceCollection.findOne(query,options);
-      res.send(result)
-    })
+      const result = await serviceCollection.findOne(query, options);
+      res.send(result);
+    });
 
-        //(post method ==creating )insert a document in server side
-        app.post('/checkout',async(req,res)=>{
-          const checkout =req.body;
-          console.log(checkout)
-          const result =await checkoutCollection.insertOne(checkout)
-          res.send(result)
-        }) 
+    //Check out portion in server side
+    app.get("/checkout", async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await checkoutCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //(post method ==creating )insert a document in server side
+    app.post("/checkout", async (req, res) => {
+      const checkout = req.body;
+      console.log(checkout);
+      const result = await checkoutCollection.insertOne(checkout);
+      res.send(result);
+    });
+    
+    //api for updation with patch method
+    app.patch('/checkout/:id',async(req,res)=>{
+      const id =req.params.id;
+      const filter ={_id: new ObjectId(id)};
+      const updateCheckout =req.body;
+      console.log(updateCheckout);
+      const updateDoc = {
+        $set: {
+          status: updateCheckout.status
+        },
+      };
+      const result =await checkoutCollection.updateOne(filter,updateDoc);
+      res.send(result);
+    })
+//api for delete operation for delete in checkout part
+    app.delete('/checkout/:id',async(req,res)=>{
+      const id =req.params.id;
+      const query ={_id:new ObjectId(id)}
+      const result =await checkoutCollection.deleteOne(query);
+      res.send(result);
+    })
+    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -76,14 +109,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("doctor is running");
+});
 
-
-
-
-app.get('/',(req,res)=>{
-    res.send('doctor is running')
-})
-
-app.listen(port,()=>{
-    console.log(`Car Doctor Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Car Doctor Server is running on port ${port}`);
+});
